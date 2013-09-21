0970bbdb29bc2b37a660c4f8cc874f7313532aec
Nathan Burgers <nateburgers@gmail.com>
2013-09-21 10:22:49 -0400

    Initial Commit

---
+++ SnarkDemo/SDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SDAppDelegate.h
+//  SnarkDemo
+//
+//  Created by Nathan Burgers on 9/21/13.
+//  Copyright (c) 2013 Nathan Burgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SnarkDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SnarkDemo
+//
+//  Created by Nathan Burgers on 9/21/13.
+//  Copyright (c) 2013 Nathan Burgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SDAppDelegate class]));
+    }
+}
